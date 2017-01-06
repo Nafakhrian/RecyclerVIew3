260b823f9f7375e159a9c0efa551f835591de471
Naufal <naufalfakhrian9@gmail.com>
2017-01-06 08:44:34 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.learn.recyclerview3.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Win10 on 1/6/2017.
+ */
+
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+
+}
